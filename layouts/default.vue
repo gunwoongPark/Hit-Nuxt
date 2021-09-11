ff43ea21cc74f260513038ca08915ca6703874b9
parkGunWoong <bung1438@gmail.com>
2021-09-11 20:11:19 +0900

    nuxt-link 태그 소개 기존의 router-link말고 nuxt-link를 사용한다. 즉 router-view 는 Nuxt 에 대응 되고 router-link는 nuxt-link에 대응된다.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <h1>{{ $route.name }}페이지</h1>
+    <header>
+      <h1>{{ $route.name }}페이지</h1>
+    </header>
+    <NuxtLink to="/">홈 페이지</NuxtLink>
+    <NuxtLink to="/main">메인 페이지</NuxtLink>
+    <NuxtLink to="/product">상품 페이지</NuxtLink>
     <Nuxt />
   </div>
 </template>
