1515c4ddcd490b8f17b416bede93dc816800727d
parkGunWoong <bung1438@gmail.com>
2021-09-11 20:08:01 +0900

    레이아웃 컴포넌트 소개 레이아웃 컴포넌트에서 공통적으로 쓰이는 헤더와 같은 컴포넌트등을 정의하고 콘텐츠 자체를 page 컴포넌트에 넣는다.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,6 @@
 <template>
-  <Nuxt />
+  <div>
+    <h1>{{ $route.name }}페이지</h1>
+    <Nuxt />
+  </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,29 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>홈 페이지</h1>
+    <p>넉스트 시작하기</p>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'LearnNuxtIndex',
+
+  data () {
+    return {
+
+    };
+  },
+
+  mounted () {
+
+  },
+
+  methods: {
+
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+</style>
